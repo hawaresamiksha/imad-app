f56ca7d0ba08d6e9736ebcfd2432cc5c3101fbc2
hawaresamiksha <30974081+hawaresamiksha@users.noreply.github.com>
2017-08-19 22:38:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //Move the image
 
-var img= document.getElementbyId('img');
+var img= document.getElementbyId('madi');
 img.onclick=function ()
 {
     img.style.marginLeft='100px';
