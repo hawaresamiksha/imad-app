30a47dc3c88f5dc35b561d092b35b1594126daac
hawaresamiksha <30974081+hawaresamiksha@users.noreply.github.com>
2017-08-19 19:12:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.get('/article-one',function(req,res)
 {
     res.send("Article one requested and served here");
 });
+
+app.get('/article-two',function(req,res)
+{
+    res.send("Article two requested and served here");
+});
+
+app.get('/article-three',function(req,res)
+{
+    res.send("Article three requested and served here");
+});
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
