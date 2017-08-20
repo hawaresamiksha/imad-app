219bed35f8f3431de45b3ae6a5f650b4a2585bd4
hawaresamiksha <30974081+hawaresamiksha@users.noreply.github.com>
2017-08-20 23:06:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,21 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function()
 {
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function()
+    {
+        if(request.readystatechange==XMLHttpRequest.DONE)
+        {
+            if(request.status==200)
+            {
+                    var counter=request.responsetext;
+                    var span=document.getElementById('count');
+                    span.innerHTML=counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET','http://samikshahaware.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
